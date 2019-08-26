a5c1c8972b6422d2dea9ed3b361d4825989cbe0a
Joe <josephvesleywork@gmail.com>
2019-08-26 13:14:13 -0400

    dropdown was not firing correctly, so put in datalist

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import { Button, Form, Grid, Dropdown} from 'semantic-ui-react';
+import React from "react";
+import { Button, Form, Grid, Dropdown } from "semantic-ui-react";
 function Search(props) {
-
-
-  let genreOptions=[
-    {key: "c", value: 'c', text: "Comedy"},
-    {key: "ho", value: 'ho', text: "Horror"},
-    {key: "hi", value: 'hi', text: "Historical"},
-    {key: "re", value: 're', text: "Religious"},
-    {key: "sf", value: 'sf', text: "Science Fiction"},
-    {key: "ro", value: 'ro', text: "Romance"},
-
-
-
-  ]
+  let genreOptions = [
+    { key: "c", value: "c", text: "Comedy" },
+    { key: "ho", value: "ho", text: "Horror" },
+    { key: "hi", value: "hi", text: "Historical" },
+    { key: "re", value: "re", text: "Religious" },
+    { key: "sf", value: "sf", text: "Science Fiction" },
+    { key: "ro", value: "ro", text: "Romance" }
+  ];
 
   return (
     <Form>
       <Grid columns={2}>
         <Grid.Column>
           <Form.Field>
-            <Dropdown
+            <Form.Input
               name="searchText"
               type="text"
-              fluid
-              search
-              selection
-              options={genreOptions}
-              className="form-control"
               placeholder="Enter Genre"
+              // fluid
+              // search={props.searchText}
+              // selection={props.handleNext}
+              list="genreOptions"
+              className="form-control"
               value={props.searchText}
               onChange={props.handleInputChange}
               onKeyPress={props.handleKeyPress}
             />
+
+            <datalist id="genreOptions">
+              <option value="Comedy" />
+              <option value="Adventure" />
+              <option value="Science Fiction" />
+              <option value="Horror" />
+              <option value="Religious" />
+              <option value="Nonfiction" />
+              <option value="Romance" />
+              <option value="Historical" />
+            </datalist>
           </Form.Field>
         </Grid.Column>
         <Grid.Column>
