2b8e47f6529059ff79f637ec033b2b3cdba69ae3
Joe <josephvesleywork@gmail.com>
2019-08-27 10:01:29 -0400

    whole genre datalist, but not dynamically appended, need to refactor libridexer to be sure all of the terms will actually work

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -3,7 +3,12 @@ import React from 'react'
 
 function SearchBox(props)
 {
-
+return(<datalist id="genreOptions">
+{props.genres.map(
+    (genre) =>
+  <option value={genre}/>)} 
+</datalist>
+)
 }
 
 export default SearchBox;
