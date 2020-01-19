7ebe49ca6b98addb5688117b09aebe395bcd6700
Joe <josephedwardwork@gmail.com>
2020-01-18 22:48:55 -0500

    commit

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
