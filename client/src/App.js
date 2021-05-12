5aede8a2f3eb8253adce3f039aea5cfac1a8010e
josephedward <josephedwardwork@gmail.com>
2021-05-12 16:02:40 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import React
-// , { useState } 
-from "react";
+import React from "react"; // , { useState }
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Player";
 import Library from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
-import RegisterUser from "./pages/Registration"
-import SignOn from "./pages/Login"
+import RegisterUser from "./pages/Registration";
+import SignOn from "./pages/Login";
 
 //import Admin from "./pages/Admin.js"
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from "./PrivateRoute";
 // import { AuthContext } from "./context/auth";
 
 import { useAuth0 } from "./react-auth0-spa";
 import history from "./utils/history";
 
 function App(props) {
-  // const [authTokens, setAuthTokens] = useState();
-  
-
-  // const setTokens = (data) => {
-  //   localStorage.setItem("tokens", JSON.stringify(data));
-  //   setAuthTokens(data);
-  // }
 
   const { loading } = useAuth0();
 
@@ -30,25 +21,33 @@ function App(props) {
     return <div>Loading...</div>;
   }
 
-
-
-
   return (
-    // <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
-    <Router history={history} >
-      <div   >
+    <Router history={history}>
+      <div>
         <Switch>
           <Route className="fullHeight" exact path="/" component={Home} />
-          <PrivateRoute className="fullHeight"  exact path="/profile" component={Library} />
-          <Route className="fullHeight"  exact path="/login" component={SignOn} />
-          <Route className="fullHeight"  exact path="/register" component={RegisterUser} />
-          {/* REDIRECTS IF NOT LOGGED IN */}
-          {/* <PrivateRoute className="fullHeight"  exact path="/admin" component={Admin} /> */}
+          <PrivateRoute
+            className="fullHeight"
+            exact
+            path="/profile"
+            component={Library}
+          />
+          <Route
+            className="fullHeight"
+            exact
+            path="/login"
+            component={SignOn}
+          />
+          <Route
+            className="fullHeight"
+            exact
+            path="/register"
+            component={RegisterUser}
+          />
           <Route component={NoMatch} />
         </Switch>
-        </div>
+      </div>
     </Router>
-    // </AuthContext.Provider>
   );
 }
 
