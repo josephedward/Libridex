de48034f6208da7510da107688ce782bf36f17f9
Joseph <jedwards@techstyle.com>
2021-11-18 01:11:12 -0500

    remove

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from "react"; 
 import {Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Player";
-import Library from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
-import RegisterUser from "./pages/Registration";
-import SignOn from "./pages/Login";
-import Admin from "./pages/Admin.js"
 import history from "./utils/history";
 
 function App(props) {
